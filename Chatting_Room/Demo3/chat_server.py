e272fa63589b06033f4b9fb4b81e600662bf3938
MinTimmy <timmygood6590@gmail.com>
2021-06-14 12:25:24 +0800

    new

---
+++ Chatting_Room/Demo3/chat_server.py
@@ -0,0 +1,124 @@
+import socket
+import select
+from threading import *
+import sys
+import threading
+import _thread
+import threaded
+import time
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+"""
+the first argument AF_INET is the address domain of the socket. This is used when we have an Internet Domain
+with any two hosts
+The second argument is the type of socket. SOCK_STREAM means that data or characters are read in a continuous flow
+"""
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+if len(sys.argv) != 3:
+    print("Correct usage: script, IP address, port number")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+IP_address = socket.gethostbyname(socket.gethostname())
+
+Port = 8080
+server.bind((IP_address, Port)) 
+
+
+#listens for 100 active connections. This number can be increased as per convenience
+list_of_clients=[]
+
+class clientThread(threading.Thread):
+    def __init__(self, conn, addr):
+        self.conn = conn
+        self.addr = addr
+        threading.Thread.__init__(self)
+        self.socket = clientSocket
+        print("New Connection: " , self.addr[0])
+
+    def run(self):
+        print("Connection from", self.addr[0])
+        CA = clientAddress
+        message = ''
+        file = open('buffer','wb')
+        while True:
+            try:     
+                message = self.conn.recv(2048).decode('UTF-8')
+                print(message[:4]) 
+                if message[:4] == 'send':
+                    print("hello")
+                    temp = self.conn.recv(2048)
+                    while temp != b'end\n':
+                        file.write(temp)
+                        temp = self.conn.recv(2048)
+                        print(temp)
+                    print("end")
+                else:
+                    # print(message)
+                    # message = bytes(message, 'UTF-8')
+                    
+                    # print(message)
+                    # file.write(message)
+                    # print(message) 
+
+                    # time.sleep(10)
+                    print("<" + self.addr[0] + "> " + message)
+                    message_to_send = "<" + self.addr[0] + "> " + message
+                    self.broadcast_message(message_to_send,self.conn)
+                    #prints the message and address of the user who just sent the message on the server terminal 
+                                       
+                self.remove(self.conn)
+            except:
+                continue
+    def broadcast_file(self, message,connection):
+        # print("Do broadcast")
+        # print(list_of_clients)
+        for clients in list_of_clients:
+            if clients!=connection:
+                try:
+                    clients.sendall(bytes(message, 'UTF-8'))
+                except:
+                    clients.close()
+                    self.remove(clients)
+
+    def broadcast_message(self, message,connection):
+        # print("Do broadcast")
+        # print(list_of_clients)
+        for clients in list_of_clients:
+            if clients!=connection:
+                try:
+                    clients.sendall(bytes(message, 'UTF-8'))
+                except:
+                    clients.close()
+                    self.remove(clients)
+
+    def remove(self, connection):
+        if connection in list_of_clients:
+            list_of_clients.remove(connection)
+
+
+
+while True:
+    #binds the server to an entered IP address and at the specified port number. The client must be aware of these parameters
+    server.listen(100)
+    clientAddress, clientSocket = server.accept()
+    """
+    Accepts a connection request and stores two parameters, conn which is a socket object for that user, and addr which contains
+    the IP address of the client that just connected
+    """
+    list_of_clients.append(clientAddress)
+    # print(clientSocket[0] + " connected")
+    print(clientSocket[0] + " connected")
+    #maintains a list of clients for ease of broadcasting a message to all available people in the chatroom
+    #Prints the address of the person who just connected
+    # start_new_thread(clientthread,(conn,addr))
+    #creates and individual thread for every user that connects
+    # _thread.start_new_thread(clientthread,(conn,addr))
+
+    # print("hello")
+    newtread = clientThread(clientAddress, clientSocket)
+    newtread.start()
+
+conn.close()
+server.close()
---
+++ Chatting_Room/Demo3/client.py
@@ -0,0 +1,67 @@
+# from Chatting_Room.Demo1.Server import IP_address
+import socket
+import select
+import sys
+import time
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if len(sys.argv) != 3:
+    print ("Correct usage: script, IP address, port number")
+    exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+
+# IP_address = "127.0.0.1"
+# IP_address = "192.168.0.128"
+# IP_address = '172.20.10.6'
+Port = 8080
+
+server.connect((IP_address, Port))
+
+while True:
+    sockets_list = [sys.stdin, server]
+    read_sockets,write_socket, error_socket = select.select(sockets_list, [], [])
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048).decode('UTF-8')
+            if message == "message":
+                while message:
+                    message = ""
+                    message = socks.recv(2048).decode('UTF-8')
+                    print (message)
+            else:
+                file = open(message, 'wb')
+                message = ""
+                message = socks.recv(2048)
+                while message:
+                    file.write(message)
+                    message = socks.recv(2048)
+        else:
+            print("send [filename]")
+            message = sys.stdin.readline()
+            if message[0:4] == "send":
+
+                message = 'send 2.jpg\n'
+                server.sendall(bytes(message, 'UTF-8'))
+                sys.stdout.write("<You>")
+                sys.stdout.write(message)
+                sys.stdout.flush()
+                file = open(message[5:len(message)-1], 'rb')
+                imgData = file.readline(2048)
+                print("sending....")
+                while(imgData): 
+                    server.send(imgData)
+                    imgData = file.readline(2048)
+                    if not imgData:
+                        break
+                time.sleep(5)
+                print("finish")
+                server.sendall(bytes('end\n', 'UTF-8'))
+            else: 
+                server.sendall(bytes(message, 'UTF-8'))
+                sys.stdout.write("<You>")
+                sys.stdout.write(message)
+                sys.stdout.flush()
+               
+            
+server.close()
